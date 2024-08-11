9a10489b0811060f716793bc64afb9e81c9a8f2a
Nada Yousry <Nada.Yousry@linkdev.com>
2024-08-11 11:39:20 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import singleSpaReact from 'single-spa-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const lifecycles = singleSpaReact({
+  React,
+  ReactDOM,
+  rootComponent: App,
+  errorBoundary(err, info, props) {
+    return <div>Something went wrong in the Display App!</div>;
+  },
+});
+
+export const { bootstrap, mount, unmount } = lifecycles;
+
+// If you still want to run this standalone (outside of single-spa):
+if (!window.singleSpaNavigate) {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
